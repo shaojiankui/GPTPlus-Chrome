6990869f2fa60be87bd35ac209eb481d47e35715
Jakey <6077782@qq.com>
2023-11-20 16:50:46 +0800

    commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# GPTPlus-Chrome
-show ChatGPT "Upgrade to Plus" button,and pay with card
+# 使用
+1.下载代码,谷歌浏览器加载已解压的扩展程序
+2.刷新chatgpt
+3.点击"Upgrade to Plus" 即可
---
+++ background.js
@@ -0,0 +1,34 @@
+﻿
+ const tabinfo = new Map();
+ function hasCSP(headers = []) {
+   return headers.some(
+     (x) => x.name.toLowerCase() === "content-security-policy"
+   );
+ }
+ 
+chrome.webRequest.onHeadersReceived.addListener(
+   function (details) {
+     tabinfo.set(details.tabId, hasCSP(details.responseHeaders));
+   },
+   {
+     urls: ["<all_urls>"],
+     types: ["main_frame"],
+   },
+   ["responseHeaders"]
+ );
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+      var url = chrome.extension.getURL("app-070eb5460c2d5ce1.js")+ "?t="+new Date().getTime();
+      return { redirectUrl: url };
+    },
+    {
+      urls: ["*://cdn.oaistatic.com/_next/static/chunks/pages/_app-070eb5460c2d5ce1.js"], 
+      types: ["script",'stylesheet'] 
+  
+    },
+    ["blocking"] 
+  );
+chrome.tabs.onRemoved.addListener(function (tabId) {
+    tabinfo.delete(tabId);
+});
